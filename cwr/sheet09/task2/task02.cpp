5dd54b724d6894b7d8adb25f5c8f54bc6b6eee5e
FlashTek <rzrolandzimmermann@live.de>
2016-05-17 16:45:42 +0200

    Fixed CWR 09

--- cwr/sheet09/task2/task02.cpp
+++ cwr/sheet09/task2/task02.cpp
@@ -32,135 +32,84 @@ public:
     }
 };
 
-int jordan_cross_product_test(point2d a, point2d b, point2d c)
+double point_in_triangle_half_plane_sign(point2d p1, point2d p2, point2d p3)
 {
-  if (a.y == b.y && b.y == c.y)
-  {
-    if ((b.x <= a.x && a.x <= c.x) || (c.x <= a.x && a.x <= b.x))
-    {
-      return 0;
-    }
-    else
-    {
-      return 1;
-    }
-  }
-
-  if (b.y > c.y)
-  {
-    double tmp_x = b.x, tmp_y = b.y;
-    b.x = c.x;
-    b.y = c.y;
-    c.x = tmp_x;
-    c.y = tmp_y;
-  }
-
-  if (a.y == b.y || a.x == b.x)
-  {
-    return 0;
-  }
-
-  if (a.y <= b.y || a.y > c.y)
-  {
-    return 1;
-  }
-
-  double delta = (b.x-a.x) * (c.y-a.y) - (b.y-a.y) * (c.x-a.x);
-  if (delta > 0)
-  {
-    return -1;
-  }
-  else if (delta < 0)
-  {
-    return 1;
-  }
-  else{
-    return 0;
-  }
+    return (p1.x - p3.x) * (p2.y - p3.y) - (p2.x - p3.x) * (p1.y - p3.y);
 }
 
-int jordan_point_in_polygon_test(size_t n, point2d* points, point2d test_point)
+bool point_is_in_triangle(point2d point, point2d v1, point2d v2, point2d v3)
 {
-  int t = -1;
+  bool b1, b2, b3;
 
-  for (int i=0;i<n-1;i++)
-  {
-    t *= jordan_cross_product_test(test_point, points[i], points[i+1]);
-  }
+   b1 = point_in_triangle_half_plane_sign(point, v1, v2) < 0.0;
+   b2 = point_in_triangle_half_plane_sign(point, v2, v3) < 0.0;
+   b3 = point_in_triangle_half_plane_sign(point, v3, v1) < 0.0;
 
-  t *= jordan_cross_product_test(test_point, points[n-1], points[0]);
-
-  return t;
+   return ((b1 == b2) && (b2 == b3));
 }
 
 bool point_is_in_hexagon(double x, double y, double middle_x, double middle_y, double side_length)
 {
-  point2d* points = new point2d[3];
+  point2d point = point2d(x,y);
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x - side_length, middle_y);
-  points[2] = point2d(middle_x - side_length/2, middle_y + sqrt(3)/2 * side_length);
+  point2d v1 = point2d(middle_x, middle_y);
+  point2d v2 = point2d(middle_x - side_length, middle_y);
+  point2d v3 = point2d(middle_x - side_length/2, middle_y + sqrt(3)/2 * side_length);
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << x << "\t" << y << "\t" << middle_x << "\t" << middle_y << endl;
     return true;
   }
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x - side_length/2, middle_y + sqrt(3)/2 * side_length);
-  points[2] = point2d(middle_x + side_length/2, middle_y + sqrt(3)/2 * side_length);
+  v1 = point2d(middle_x, middle_y);
+  v2 = point2d(middle_x - side_length/2, middle_y + sqrt(3)/2 * side_length);
+  v3 = point2d(middle_x + side_length/2, middle_y + sqrt(3)/2 * side_length);
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << "2\t";
     return true;
   }
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x + side_length/2, middle_y + sqrt(3)/2 * side_length);
-  points[2] = point2d(middle_x + side_length, middle_y);
+  v1 = point2d(middle_x, middle_y);
+  v2 = point2d(middle_x + side_length/2, middle_y + sqrt(3)/2 * side_length);
+  v3 = point2d(middle_x + side_length, middle_y);
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << "3\t";
     return true;
   }
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x + side_length, middle_y);
-  points[2] = point2d(middle_x + side_length/2, middle_y - sqrt(3)/2 * side_length);
+  v1 = point2d(middle_x, middle_y);
+  v2 = point2d(middle_x + side_length, middle_y);
+  v3 = point2d(middle_x + side_length/2, middle_y - sqrt(3)/2 * side_length);
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << "4\t";
     return true;
   }
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x - side_length/2, middle_y - sqrt(3)/2 * side_length);
-  points[2] = point2d(middle_x + side_length/2, middle_y - sqrt(3)/2 * side_length);
+  v1= point2d(middle_x, middle_y);
+  v2 = point2d(middle_x - side_length/2, middle_y - sqrt(3)/2 * side_length);
+  v3 = point2d(middle_x + side_length/2, middle_y - sqrt(3)/2 * side_length);
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << "5\t";
     return true;
   }
 
-  points[0] = point2d(middle_x, middle_y);
-  points[1] = point2d(middle_x - side_length/2, middle_y - sqrt(3)/2 * side_length);
-  points[2] = point2d(middle_x - side_length, middle_y );
+  v1 = point2d(middle_x, middle_y);
+  v2 = point2d(middle_x - side_length/2, middle_y - sqrt(3)/2 * side_length);
+  v3 = point2d(middle_x - side_length, middle_y );
 
-  if (jordan_point_in_polygon_test(3,points, point2d(x,y)))
+  if (point_is_in_triangle(point, v1, v2, v3))
   {
-    cout << "6\t";
     return true;
   }
 
   return false;
 }
 
-bool point_is_in_triangle(double x, double y, double middle_x, double middle_y, double rotation_angle, double side_length)
+bool point_is_in_triangle_arround_centre(double x, double y, double middle_x, double middle_y, double rotation_angle, double side_length)
 {
     //at first: calculate relative coordinates to the midle of the triangle, then rotate them
     point2d a_raw = point2d(0.0,sqrt(3)/2.0 * side_length/2.0);
@@ -172,10 +121,7 @@ bool point_is_in_triangle(double x, double y, double middle_x, double middle_y, 
     point2d b = point2d(middle_x + cos(rotation_angle) * b_raw.x + sin(rotation_angle) * b_raw.y, middle_y -sin(rotation_angle) * b_raw.x + cos(rotation_angle) * b_raw.y);
     point2d c = point2d(middle_x + cos(rotation_angle) * c_raw.x + sin(rotation_angle) * c_raw.y, middle_y -sin(rotation_angle) * c_raw.x + cos(rotation_angle) * c_raw.y);
 
-    point2d points[] = {a,b,c};
-
-    int result = jordan_point_in_polygon_test(3, points, point2d(x,y));
-    return result >= 0;
+    return point_is_in_triangle(point2d(x,y), a, b, c);
 }
 
 bool point_has_fixed_border_constraint(int x, int y, int x_dimension, int y_dimension, double delta_x, double delta_y)
@@ -192,7 +138,7 @@ bool point_has_fixed_border_constraint(int x, int y, int x_dimension, int y_dime
     return true;
   }
 
-  if (point_is_in_triangle(x * delta_x, y*delta_y, x_position_electrode_2, y_position_electrode_2, 0.0, size_electrode_2))
+  if (point_is_in_triangle_arround_centre(x * delta_x, y*delta_y, x_position_electrode_1, y_position_electrode_1, 0.0, size_electrode_1))
   {
     return true;
   }
@@ -284,7 +230,7 @@ int main(int argc, char* argv[])
   {
     for (int y=0; y<dimension_y; y++)
     {
-      values[x][y] = (rand() % 2000) / 1000.0 - 1.0;
+      values[x][y] = 0;//(rand() % 2000) / 1000.0 - 1.0;
     }
   }
 
@@ -309,26 +255,26 @@ int main(int argc, char* argv[])
     values[dimension_x-1][y] = 0;
   }
 
-  //electrode 1
+  //electrode 2
   for (int x = 0; x<dimension_x; x++)
   {
     for (int y=0; y<dimension_y; y++)
     {
       if (point_is_in_hexagon(x * delta_x, y*delta_y, x_position_electrode_2, y_position_electrode_2, size_electrode_2))
       {
-        values[x][y] = v_electrode_1;
+        values[x][y] = v_electrode_2;
       }
     }
   }
 
-  //electrode 2
+  //electrode 1
   for (int x = 0; x<dimension_x; x++)
   {
     for (int y=0; y<dimension_y; y++)
     {
-      if (point_is_in_triangle(x * delta_x, y*delta_y, x_position_electrode_2, y_position_electrode_2, 0.0, size_electrode_2))
+      if (point_is_in_triangle_arround_centre(x * delta_x, y*delta_y, x_position_electrode_1, y_position_electrode_1, 0.0, size_electrode_1))
       {
-        values[x][y] = v_electrode_2;
+        values[x][y] = v_electrode_1;
       }
     }
   }
@@ -350,15 +296,18 @@ int main(int argc, char* argv[])
     totalIterations++;
   }
 
-  //calc E
+  //print the results
   for (int x = 0; x<dimension_x-1; x++)
   {
     for (int y = 0; y<dimension_y-1; y++)
     {
       //outputFile << x * delta_x << "\t" << y * delta_y << "\t" << (values[x+1][y] - values[x][y]) << "\t" << (values[x][y+1] - values[x][y]) << endl;
-      outputFile << x << "\t" << y << "\t" << values[x][y] << endl;
+      outputFile << x * delta_x << "\t" << y * delta_y << "\t" << values[x][y] << endl;
     }
   }
 
   cout <<"final iteration: " << totalIterations  << "\tfinal error: " << err << endl;
 }
+
+
+//best s = 1.79 - seems to minimize the iterations
